1c0e0e47d9febee5246fc97d34788e31715bf02c
qazwsx5381 <qazwsx5381@gmail.com>
2023-09-11 17:24:02 +0900

    230911

--- vite/src/components/introducePort.vue
+++ vite/src/components/introducePort.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <div class="title"><h3 class="title">Introduce</h3></div>
+    <span>처음 접한 프로그래밍 언어는 C언어입니다.</span>
     <span
-      >개발자가 되기 위해 동성인재개발교육원에서 Tools에 표기된 FrontEnd와
-      BackEnd를 함께 배웠습니다.
+      >취업한 뒤에도 진로 고민을 계속 하다가 개발자가 되어야겠다고 마음을
+      먹었습니다.</span
+    >
+    <span
+      >그래서 동성인재개발교육원에서 Tools에 표기된 FrontEnd와 BackEnd를 함께
+      배웠습니다.
     </span>
     <span>앞으로 배워야 할 것이 많지만 열심히 공부하며 배우겠습니다.</span>
   </div>
